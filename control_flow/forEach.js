21b3116a07c9af6dab47c3a0d78ab751bd11445e
Ishan Sharma <112118500+S-Ishan@users.noreply.github.com>
2023-11-15 19:22:05 +0000

    forEach

---
+++ control_flow/forEach.js
@@ -0,0 +1,20 @@
+const coding = ["js", "ruby", "java", "python"]
+
+coding.forEach( function (item) {
+    // console.log(item);
+} )
+
+coding.forEach((item) => {
+    // console.log(item)
+} )
+
+function printMe (item) {
+    // console.log(item);
+}
+
+coding.forEach(printMe)
+
+coding.forEach( (item, index, arr) => {
+    // console.log(item, index, arr);
+})
+
