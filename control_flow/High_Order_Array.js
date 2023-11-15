3f97b8b9e7897d2aef3211dff609ddbce8ee0391
Ishan Sharma <112118500+S-Ishan@users.noreply.github.com>
2023-11-15 19:06:07 +0000

    for of

--- control_flow/High_Order_Array.js
+++ control_flow/High_Order_Array.js
@@ -3,5 +3,28 @@
 const arr = [1,2,3,4,5]
 
 for(const num of arr) {
-    console.log(num);
+    // console.log(num);
+}
+
+const greetings = "Hello World"
+
+for( const greet of greetings) {
+    // console.log(greet);
+}
+
+//===========MAPS=========
+//Unique values, Maintains Order
+
+const map = new Map()
+map.set('IN', "India")
+map.set('UN', "United Nations")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+console.log(map);
+ 
+//iteration maps using FOR OF
+for (const [Key, value] of map) { //destructure array
+    console.log(Key, value );
+    
 }
