82e0ea1aba2a66b10bcc156bad463698ee31460b
Ishan Sharma <112118500+S-Ishan@users.noreply.github.com>
2023-11-19 20:39:04 +0000

    commit

--- control_flow/forEach.js
+++ control_flow/forEach.js
@@ -18,3 +18,8 @@ coding.forEach( (item, index, arr) => {
     // console.log(item, index, arr);
 })
 
+//====================MAP REDUCE===================
+//     TO return Values
+const myNums = [1,2,3,4,5,6,,8,9,10]
+const newNums = myNums.filter( (nums) => nums > 4)
+console.log(newNums);
